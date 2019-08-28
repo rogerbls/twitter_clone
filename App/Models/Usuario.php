954342ccd5bbce6ffe7105c2e36fb492590ad7d1
Roginho <rogerbls51@gmail.com>
2019-08-27 22:18:16 -0300

    Primeira versão do projeto

---
+++ App/Models/Usuario.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Models;
+
+use MF\Model\Model;
+
+class Usuario extends Model {
+  
+    private $id;
+    private $nome;
+    private $email;
+    private $senha;
+
+    public function __get($atributo){
+        return $this->$atributo;
+    }
+    public function __set($atributo, $valor){
+        $this->$atributo = $valor;
+    }
+    
+    public function salvar(){
+        $query = "INSERT INTO usuarios (nome, email, senha) VALUES(:nome, :email, :senha)";
+        $stmt = $this->db->prepare($query);
+        $stmt->bindValue(':nome', $this->__get('nome'));
+        $stmt->bindValue(':email', $this->__get('email'));
+        $stmt->bindValue(':senha', $this->__get('senha'));
+        $stmt->execute();
+        return $this;
+    }
+
+    public function validarCadastro(){
+        $valido = false;
+        if(strlen($this->__get('nome'))<3){
+            $valido = false;
+        }else if(strlen($this->__get('email'))<3){
+            $valido = false;
+        }else if (strlen($this->__get('senha'))<3){
+            $valido = false;
+        }else{
+            $valido = true;
+        }
+        return $valido;
+    }
+    public function getUsuarioPorEmail(){
+        $query = "SELECT nome, email FROM usuarios WHERE email = :email";
+        $stmt = $this->db->prepare($query);
+        $stmt->bindValue(':email', $this->__get('email'));
+        $stmt->execute();
+        $usuario = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+        return $usuario;
+    }
+
+    public function autenticar(){
+        $query = "SELECT id, nome, email FROM usuarios WHERE email = :email AND senha =:senha ";
+        $stmt = $this->db->prepare($query);
+        $stmt->bindValue(':email', $this->__get('email'));
+        $stmt->bindValue(':senha', $this->__get('senha'));
+        $stmt->execute();
+        $usuario = $stmt->fetch(\PDO::FETCH_ASSOC);
+        if($usuario['id'] != '' && $usuario['nome'] != ''){
+            $this->__set('id', $usuario['id']);
+            $this->__set('nome', $usuario['nome']);
+        }
+        return $this;
+    }
+} 
+?>
